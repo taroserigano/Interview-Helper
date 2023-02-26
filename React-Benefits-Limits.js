cef84ff5aa2592b2360b975cadd34cf08a0cd5c5
Taro Serigano <taroserigano@gmail.com>
2023-02-26 16:42:57 -0600

    Create React-Benefits-Limits.js

---
+++ React-Benefits-Limits.js
@@ -0,0 +1,38 @@
+
+
+
+Benefits of React: 
+
+1. development speed
+
+-intuitive and super flexible, makes developers lives easier 
+-code is simple to maintain and update, easy to scale, the best framework to create complex online applications 
+
+
+2. Reusable Components
+
+-can use the same components over and over again. 
+-can also customize these components for deisgn's needs 
+
+3. Speed 
+
+Very fast rendering speed, loading speed 
+these are remakrble compared to other frameworks 
+
+3. SEO-friendly 
+
+-SEO-optimized
+-Fast render and loading speed helps the website to rank on higher positions 
+
+
+Limits of React: 
+
+1. Only library, not framework. It is only UI components and needs bandend to support 
+
+2. Lack of updated docs - keeps updating and evolving 
+
+3. Fast updating - constantly release new updates 
+
+4. client-side rendeing by default 
+
+
