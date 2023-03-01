6f0ead3a98146e8f42c48f33adb10d0137326f8f
Taro Serigano <taroserigano@gmail.com>
2023-02-28 18:39:34 -0600

    Create React.js

---
+++ React.js
@@ -0,0 +1,21 @@
+What is React? 
+
+-It supports server-side rendering 
+-It will make use of virtual DOM rathern than the real DOM 
+-
+
+
+
+JSX
+
+-JSX stands for JavaScript XML. It allows to use to write HTML inside JavaScript and place them in te DOM. 
+
+It is also syntax sugar for React.createElemen() function. 
+
+
+Custom Hooks 
+
+-is a special React function whose name starts with the "use" can create your own hooks. 
+-you can choose whatever the arguments you use and whatever the returns you can specify
+-always starts with "use" 
+
