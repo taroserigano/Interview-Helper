e1d3e14cbfb44fcf4f0a9d1b86306f6fe71e06c5
Taro Serigano <taroserigano@gmail.com>
2023-03-13 10:57:18 -0500

    Create Twitter-API.js

---
+++ Twitter-API.js
@@ -0,0 +1,26 @@
+----TWITTER API ------
+
+
+AUTHENTICATION 
+
+-when login or signup, generates a token that gets stored in cookie 
+-Store the cookie in header whenever you send HTTP req 
+
+
+
+API THROTTLING-API Rate Limitter 
+
+-use API Gateway 
+
+API Gateway throttles requests to your API using the token bucket algorithm, where a token counts for a request. 
+
+
+
+
+AWS throttling limits are applied across all accounts and clients in a region.
+
+AWS WAF - Web Application Firewall 
+
+set the limit between 100 - 50 million times per 5 mins, can detect and specify the IP address each time 
+
+
