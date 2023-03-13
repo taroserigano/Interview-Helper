676119f61c7caef0da203e55f2bbf071bdfaa6e7
Taro Serigano <taroserigano@gmail.com>
2023-03-12 20:21:43 -0500

    Create Microservices-on-AWS.js

---
+++ Microservices-on-AWS.js
@@ -0,0 +1,16 @@
+--MICROSERVICES---
+
+ELB or API Gateway 
+--------------------
+EC2 EC2  EC2 
+
+or Lambda on serverless 
+
+store/get, store/post, store/delete 
+
+or EKS, EKS, EKS   
+
+
+or you can combine of above three services 
+
+
